1fb7e1fb87b72c52e37fdba4df49ca76b3c60a5a
Mridul5300 <mridulsarkar1157@gmail.com>
2024-05-11 18:58:21 +0600

    details page done

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
 
 // midleWare 
-app.use(cors())
-app.use(express.json())
-
-
-console.log(process.env.DB_PASS)
-
+const  corsOptions = {
+     origin: [
+          'http://localhost:5173'
+     ],
+     Credentials: true,
+     // optionSuccessStatus :200
+}
+app.use(cors(corsOptions))
+app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1mv6arg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -29,12 +32,31 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+     const hotelService = client.db('hotelBoking').collection('user');
+
+
+
+     app.get('/user', async(req, res) => {
+          const reuslt = await hotelService.find().toArray();
+          res.send(reuslt)
+     })
+
+    //  detail page kaj
+  app.get('/roomdetails/:id', async(req, res) => {
+    const id = req.params.id;
+    const query = { _id: new ObjectId(id) };
+    const result = await hotelService.findOne(query);
+    res.send(result);
+});
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+//     await client.close();
   }
 }
 run().catch(console.dir);
