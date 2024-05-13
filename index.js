363e34cac01d9dd3b0e845a3c296e40e829232cc
Mridul5300 <mridulsarkar1157@gmail.com>
2024-05-14 02:06:01 +0600

    my booking done

--- index.js
+++ index.js
@@ -6,12 +6,13 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // midleWare 
-const  corsOptions = {
-     origin: [
-          'http://localhost:5173'
-     ],
-     Credentials: true,
-     // optionSuccessStatus :200
+const corsOptions = {
+  origin: [
+    'http://localhost:5173',
+    "http://localhost:5174"
+  ],
+  Credentials: true,
+  // optionSuccessStatus :200
 }
 app.use(cors(corsOptions))
 app.use(express.json());
@@ -33,39 +34,67 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-     const hotelService = client.db('hotelBoking').collection('user');
+    const hotelService = client.db('hotelBoking').collection('user');
+    const bookingCollection = client.db('hotelBoking').collection('bookings')
+    
 
 
-
-     app.get('/user', async(req, res) => {
-          const reuslt = await hotelService.find().toArray();
-          res.send(reuslt)
-     })
+      app.get('/user', async (req, res) => {
+        const reuslt = await hotelService.find().toArray();
+        res.send(reuslt)
+      })
+  
+  
 
     //  detail page kaj
-  app.get('/roomdetails/:id', async(req, res) => {
-    const id = req.params.id;
-    const query = { _id: new ObjectId(id) };
-    const result = await hotelService.findOne(query);
-    res.send(result);
-});
+    app.get('/roomdetails/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await hotelService.findOne(query);
+      res.send(result);
+    });
 
+    // review api
+    app.get('/roomdetail/:id', async (req, res) => {
+      const roomId = req.params.id;
+      const query = ({_id: new ObjectId(roomId) })
+      const reviews = await reviewService.find(query).toArray();
+      res.send(reviews);
+    });
 
+    // booking api
+    app.get('/bookings', async (req, res) => {
+      console.log(req.query.email);
+      let query ={};
+      if(req.query?.email){
+          query = {email: req.query.email}
+      }
+      const reuslt = await bookingCollection.find(query).toArray();
+      res.send(reuslt);
+    })
+
+    app.post('/bookings', async (req, res) => {
+      const booking = req.body;
+      console.log(booking);
+      const result = await bookingCollection.insertOne(booking);
+      res.send(result);
+    });
+    
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-//     await client.close();
+    //     await client.close();
   }
 }
 run().catch(console.dir);
 
 
-app.get('/', (req,res) =>{
-     req.send('hotel server sart')
+app.get('/', (req, res) => {
+  req.send('hotel server sart')
 })
 
 app.listen(port, () => {
-     console.log('hotel is sever ready');
+  console.log('hotel is sever ready');
 })
